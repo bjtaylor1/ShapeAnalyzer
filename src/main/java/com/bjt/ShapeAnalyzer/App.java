888293744851735b0661a1cd5998328e07e5f9b7
Ben Taylor <ben.taylor@ssp-uk.com>
2015-08-08 13:49:48 +0100

    connects to postgres

--- src/main/java/com/bjt/ShapeAnalyzer/App.java
+++ src/main/java/com/bjt/ShapeAnalyzer/App.java
@@ -1,13 +1,30 @@
 package com.bjt.ShapeAnalyzer;
 
+import org.postgresql.PGConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        try {
+            Class.forName("org.postgresql.Driver");
+            String connStr = "jdbc:postgresql://192.168.1.23:5432/junctions";
+            System.out.println("Connecting...");
+            try (final Connection connection = DriverManager.getConnection(connStr, "osm", "osm")) {
+
+                System.out.println("Connected to postgres.");
+                final PGConnection pgConnection = (PGConnection) connection;
+                pgConnection.addDataType("geometry", Class.forName("org.postgis.PGgeometry"));
+            }
+        }catch(Exception e) {
+            e.printStackTrace(System.err);
+            System.exit(1);
+        }
     }
 }
