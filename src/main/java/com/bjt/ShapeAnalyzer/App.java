eba0f14ca53236393d176ea4509f4c0e108273f9
Ben Taylor <ben.taylor@ssp-uk.com>
2015-08-09 10:54:47 +0100

    reads lat longs from gpx

--- src/main/java/com/bjt/ShapeAnalyzer/App.java
+++ src/main/java/com/bjt/ShapeAnalyzer/App.java
@@ -1,19 +1,29 @@
 package com.bjt.ShapeAnalyzer;
 
 import org.postgresql.PGConnection;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App 
 {
 	public static void main( String[] args ) {
 		try {
-			loadLib("com_bjt_ShapeAnalyzer_App.so");
-			loadLib("libproj.so");
-
-			double[] latlongs = new double[]{53.231787, -1.455071,   53.238069, -1.558337 };
+			//loadLib("com_bjt_ShapeAnalyzer_App.so");
+			Double[] latlongs = readLatLongsFromGpxFile(new File(args[0]));
 			double maxPercentage = getmaxpercentage(latlongs);
 			System.out.println(String.format("The result is %.6f", maxPercentage));
 			/*            
@@ -33,11 +43,31 @@ public class App
 		}
 	}
 
+	private static Double[] readLatLongsFromGpxFile(File file) throws ParserConfigurationException, IOException, SAXException, XPathExpressionException {
+		final DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		final DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		final Document document = documentBuilder.parse(file);
+		final XPathFactory xPathFactory = XPathFactory.newInstance();
+		final XPath xPath = xPathFactory.newXPath();
+		final XPathExpression xPathExpression = xPath.compile("gpx/trk/trkseg/trkpt");
+		final NodeList nodes = (NodeList) xPathExpression.evaluate(document, XPathConstants.NODESET);
+		final List<Double> doubles = new ArrayList<>();
+		for(int i = 0; i < nodes.getLength(); i++) {
+			final Node node = nodes.item(i);
+			final NamedNodeMap attributes = node.getAttributes();
+			Double lat = Double.parseDouble(attributes.getNamedItem("lat").getNodeValue());
+			Double lon = Double.parseDouble(attributes.getNamedItem("lon").getNodeValue());
+			doubles.add(lat);
+			doubles.add(lon);
+		}
+		return doubles.toArray(new Double[]{});
+	}
+
 	private static void loadLib(final String lib) {
 		final String curdir = System.getProperty("user.dir");
 		final File libFile = new File(curdir, lib);
 		System.load(libFile.getAbsolutePath());
 	}
 
-	public static native double getmaxpercentage(double[] latLongs);
+	public static native double getmaxpercentage(Double[] latLongs);
 }
